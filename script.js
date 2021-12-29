e7d1b13b6e0fdcb8aa60fd08749505e819771c1d
placido <placidobb.ac@gmail.com>
2021-12-28 22:19:22 -0500

    Ajustes finais no projeto versao 01

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 function Converter() {
-    var nota1 = parseInt(document.getElementById("nota1").value);
-    var nota2 = parseInt(document.getElementById("nota2").value);
-    var media = (nota1 + nota2)/2; 
+
+    const nota1 = parseInt(document.getElementById("nota1").value);
+    const nota2 = parseInt(document.getElementById("nota2").value);
+    const media = (nota1 + nota2)/2; 
     
-    var calculaMedia = document.getElementById("exibirMedia");
-     var mensgeMedia = "A média é : " + media;
-    calculaMedia.innerHTML = mensgeMedia;
-   
+    const calculaMedia = document.getElementById("exibirMedia");
     
-  }
+      if(media >= 7){
+        const mensgeMedia = "A sua média foi : " + media + ", Parabéns !!! Você está aprovado";
+        calculaMedia.innerHTML = mensgeMedia;
+      
+      }if(media < 7) {
+        const mensgeMedia = "A média foi : " + media + " Você está reprovado";
+        calculaMedia.innerHTML = mensgeMedia;
+      }
+    
+
+}
